fa9baed11bc7564e116064aba854930f4f67650e
Guowei Lv <guowei@tuxera.com>
2018-04-20 11:39:31 +0300

    displaying individual albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
-import {Text, View} from "react-native";
+import {View} from "react-native";
+
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
 
@@ -13,11 +15,12 @@ class AlbumList extends Component {
     }
 
     renderAlbums() {
-        return this.state.albums.map(album => <Text>{album.title}</Text>)
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album}/>
+        )
     }
 
     render() {
-        console.log(this.state.albums);
         return (
             <View>
                 {this.renderAlbums()}
