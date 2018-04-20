15678ab5852a7a5e4126ca2acb3bd581dedb35ee
Guowei Lv <guowei@tuxera.com>
2018-04-20 10:59:21 +0300

    component level state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,12 +4,15 @@ import {Text, View} from "react-native";
 
 class AlbumList extends Component {
 
+    state = {albums: []};
+
     componentWillMount() {
         fetch("https://rallycoding.herokuapp.com/api/music_albums")
-            .then((response) => console.log(response.json()))
+            .then((response) => this.setState({albums: response.json()}))
     }
 
     render() {
+        console.log(this.state)
         return (
             <View>
                 <Text>Album List</Text>
