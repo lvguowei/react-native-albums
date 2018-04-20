481bfb0058cd7d4c31925a33fd6fadfeae054b94
Guowei Lv <guowei@tuxera.com>
2018-04-20 15:11:45 +0300

    making content scrollable

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList'
 
 const App = () => (
-        <View>
-            <Header headerText={'Albums'}/>
-            <AlbumList/>
-        </View>
+    <View style={{flex: 1}}>
+        <Header headerText={'Albums'}/>
+        <AlbumList/>
+    </View>
 
-    );
+);
 
 AppRegistry.registerComponent('albums', () => App);
