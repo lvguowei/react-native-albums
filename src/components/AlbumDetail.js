859f48865f37f65828d251c9c50226be208a7e47
Guowei Lv <guowei@tuxera.com>
2018-04-23 14:34:40 +0300

    linking between apps

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image, image} = album;
+    const {title, artist, thumbnail_image, image, url} = album;
     const {thumbnailStyle, headerTextStyle, headerContentStyle, thumbnailContainerStyle, imageStyle} = styles;
     return (
         <Card>
@@ -24,7 +24,7 @@ const AlbumDetail = ({album}) => {
                 <Image style={imageStyle} source={{uri: image}}/>
             </CardSection>
             <CardSection>
-                <Button onPress={() => console.log(title)}/>
+                <Button onPress={() => Linking.openURL(url)}/>
             </CardSection>
         </Card>
 
