9982edff6cf4ff43f78c89e7c7abd8cf99a7cc17
Guowei Lv <guowei@tuxera.com>
2018-05-07 14:52:18 +0300

    more on handling user input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -3,11 +3,17 @@ import {TextInput} from 'react-native';
 import {Button, Card, CardSection} from "./common";
 
 class LoginForm extends Component {
+
+    state = {text: ''};
+
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput style={{height: 20, width: 100}}/>
+                    <TextInput
+                        style={{height: 20, width: 100}}
+                        value={this.state.text}
+                        onChangeText={text => this.setState({text: text})}/>
                 </CardSection>
                 <CardSection/>
                 <CardSection>
