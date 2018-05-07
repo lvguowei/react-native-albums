52d6faf9ab1ae30d73ce50c902c6a5c4c416da72
Guowei Lv <guowei@tuxera.com>
2018-05-07 14:25:26 +0300

    firebase client setup

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import firebase from 'firebase'
 import {Header} from "./components/common";
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyA0E7qLaENlMbgD0udRer5VTp1yEWp2aQM",
+            authDomain: "reactnative-auth-3d606.firebaseapp.com",
+            databaseURL: "https://reactnative-auth-3d606.firebaseio.com",
+            projectId: "reactnative-auth-3d606",
+            storageBucket: "reactnative-auth-3d606.appspot.com",
+            messagingSenderId: "292831521934"
+        })
+    }
+
     render() {
         return (
             <View>
